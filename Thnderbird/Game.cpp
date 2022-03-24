ed3a714cfa1f5be8815d201790d92021f094d8bf
talym <talym@mta.ac.il>
2022-03-24 11:23:31 +0200

    Switch ship movement still need to set stoppoing ship when click on b or s

--- Thnderbird/Game.cpp
+++ Thnderbird/Game.cpp
@@ -49,27 +49,43 @@ void Game::run() {
 //	cout << "Game is running !" << endl;
 	char key = 0;
 	int dir;
+	bool isBigMove = true;
 	do {
-		/*if (_kbhit())
-		{
-			key = _getch();
-			if ((dir = smallShip.getDirection(key)) != -1)
-				smallShip.setDirection(dir);
-			else if ((dir = smallShip.getDirection(key)) != -1)
-				smallShip.setDirection(dir);	
+		if (isBigMove) {
+			if (_kbhit())
+			{
+				key = _getch();
+				if (key == 's' || key == 'S') {
+					isBigMove = false;
+				}
+				else {
+					if ((dir = bigShip.getDirection(key)) != -1)
+						bigShip.setDirection(dir);
+					else if ((dir = bigShip.getDirection(key)) != -1)
+						bigShip.setDirection(dir);
+				}
+			}
+			bigShip.move(bigShip.getType());
+			Sleep(400);
 		}
-		smallShip.move(smallShip.getType());
-		Sleep(400);*/
-		if (_kbhit())
+		if (!isBigMove)
 		{
-			key = _getch();
-			if ((dir = bigShip.getDirection(key)) != -1)
-				bigShip.setDirection(dir);
-			else if ((dir = bigShip.getDirection(key)) != -1)
-				bigShip.setDirection(dir);
+			if (_kbhit())
+			{
+				key = _getch();
+				if (key == 'b' || key == 'B') {
+					isBigMove = true;
+				}
+				else {
+					if ((dir = smallShip.getDirection(key)) != -1)
+						smallShip.setDirection(dir);
+					else if ((dir = smallShip.getDirection(key)) != -1)
+						smallShip.setDirection(dir);
+				}
+			}
+			smallShip.move(smallShip.getType());
+			Sleep(400);
 		}
-		bigShip.move(bigShip.getType());
-		Sleep(400);
 	} while (key != ESC);
 	pause(); 
 	//should be handle ship movement too 
@@ -87,13 +103,13 @@ void Game::init() {
 	bigShip = SpaceShip(2, 2, '#', Color::GREEN);
 	bigShip.setType(2);
 	bigShip.setMat(bigShip.getType());
-	bigShip.setArrowKeys("wsad");
+	bigShip.setArrowKeys("wxad");
 	
 	//char smallKeys[4] = { 'U','J', 'H', 'K' };
 	smallShip = SpaceShip(1, 2, '@', Color::BLUE);
 	smallShip.setType(1);
 	smallShip.setMat(smallShip.getType());
-	smallShip.setArrowKeys("ujhk");
+	smallShip.setArrowKeys("wxad");
 
 	smallShip.initDraw(smallShip.getType());
 	//smallShip.move(smallShip.getType());*/
