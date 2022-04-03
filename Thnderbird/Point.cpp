efd91cc194c6b01e96eac5eb72db1bab0abd96bd
talym <talym@mta.ac.il>
2022-04-04 00:43:15 +0300

    Point DOCS

--- Thnderbird/Point.cpp
+++ Thnderbird/Point.cpp
@@ -1,18 +1,26 @@
 #include "Point.h"
 
-
+/*
+Constructor of Point with 2 parameters.
+*/
 Point::Point(int _x, int _y)
 {
 	x = _x;
 	y = _y;
 };
 
+/*
+Constructor of Point with 3 parameters.
+*/
 Point::Point(int _x, int _y, char _fig)
 {
 	x = _x;
 	y = _y;
 	figure = _fig;
 }
+/*
+Constructor of Point with 5 parameters.
+*/
 Point::Point(int _x, int _y, char _fig, Color _color, int _objectId)
 {
 	x = _x;
@@ -23,24 +31,40 @@ Point::Point(int _x, int _y, char _fig, Color _color, int _objectId)
 }
 ;
 
+/*
+This is setter function of x data member.
+*/
 void Point::setX(int _x) {
 	x = _x;
 
 }
+
+/*
+This is setter function of y data member.
+*/
 void Point::setY(int _y) {
 	y = _y;
 }
 
+/*
+This is getter function of x data member.
+*/
 int Point::getX() const
 {
 	return x;
 }
 
+/*
+This is getter function of y data member.
+*/
 int Point::getY() const
 {
 	return y;
 }
 
+/*
+This function is used to make point movement according to direction.
+*/
 void Point::move(int dir) {
 	switch (dir) {
 	case 0: // UP
@@ -58,35 +82,56 @@ void Point::move(int dir) {
 	}
 }
 
+/*
+This is getter function of figure data member.
+*/
 char Point::getFigure() const
 {
 	return figure;
 }
 
+/*
+This function is used to draw point.
+*/
 void Point::draw() const {
 	gotoxy(x, y);
 	setTextColor(color);
 	cout << figure << endl;
 }
 
+/*
+This function is used to dreaw point with figure parameter.
+*/
 void Point::draw(char _fig)
 {
 	gotoxy(x, y);
 	cout << _fig << endl;
 }
 
+/*
+This is setter function of figure data member.
+*/
 void  Point::setFigure(char _figure) {
 	figure = _figure;
 }
 
+/*
+This is setter function of object id data member.
+*/
 void Point::setObjecId(int _objectId) {
 	objectId = _objectId;
 }
 
+/*
+This is getter function of object id data member.
+*/
 int Point::getObjecId() const{
 	return objectId;
 }
 
+/*
+Distructor of Point.
+*/
 Point::~Point() {
 
 }
