d05a4005b1b44828e3b10560fb4da5962a0a5b0b
talym <talym@mta.ac.il>
2022-04-03 20:19:55 +0300

    Changed main function to void and add docs

--- Thnderbird/main.cpp
+++ Thnderbird/main.cpp
@@ -1,11 +1,15 @@
 #include "Game.h"
 
-int main()
+
+/*
+This is main function of Thunderbirds program.
+Function creates Game object. Function performs color mode selection and then starts the game.
+*/
+void main()
 {
 
 	Game game;
 	game.selectColorMode();
 	game.start();
-
 
 }
