1a267bdf0c815b61b97275fe07780e60254a13f9
idobi <idobi@mta.ac.il>
2022-06-01 00:52:56 +0300

    asdf

--- Thnderbird/GameMode.h
+++ Thnderbird/GameMode.h
@@ -5,16 +5,16 @@ enum class GameMode
 {
 	LOAD = 0,
 	SAVE = 1,
-	ERROR = 2
+	ERROR_MODE = 2
 	
 };
 
 inline GameMode getMode(std::string& mode)
 {
-	if (mode == "load")
+	if (mode == LOAD_MOAD)
 		return GameMode::LOAD;
-	else if (mode == "save")
+	else if (mode == "SAVE_MOAD")
 		return GameMode::SAVE;
 	else
-		return GameMode::ERROR;
+		return GameMode::ERROR_MODE;
 }
